9243bc0ae01f0c0ff343a7ebe293c4ae724f438f
gbbe-mservices <karleb04@gmail.com>
2026-09-05 07:20:34 +0200

    Refactor server.js for improved readability

--- server.js
+++ server.js
@@ -77,5 +77,5 @@ app.get('*', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(--- Serveur GBBE MULTI-SERVICES prêt sur le port ${PORT} ---`);
+    console.log(`--- Serveur GBBE MULTI-SERVICES prêt sur le port ${PORT} ---`);
 });
