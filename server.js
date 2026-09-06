6994ea8f125c89fcbdd35a0d009aa6d3535ab479
gbbe-mservices <karleb04@gmail.com>
2026-09-06 15:19:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware pour décoder le JSON et les formulaires
+// Permet de lire les données JSON et formulaires
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Servir les fichiers statiques (index.html, style.css, app.js, etc.)
+// Sert les fichiers statiques (index.html, style.css, app.js, etc.)
 app.use(express.static(path.join(__dirname)));
 
 // Route principale
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Lancement du serveur
+// Démarrage du serveur
 app.listen(PORT, () => {
-  console.log(`Serveur GBBE en cours d'exécution sur le port ${PORT});
+  console.log(`Serveur GBBE opérationnel sur le port ${PORT}`);
 });
